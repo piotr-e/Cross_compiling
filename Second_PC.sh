4c242e2eec65270065395f994d59ac48aa9b5841
piotr-e <piotr-e@wp.pl>
2012-12-03 21:10:57 +0100

    Update Second_PC.sh

--- Second_PC.sh
+++ Second_PC.sh
@@ -2,48 +2,46 @@
 
 echo "Hello $USER."
 echo "This is script, which it compiles your Raspbian kernel."
-echo "You must log as superuser."
 echo "Plug your pendrive with .config file to your PC."
 echo "Give my path to your pendrive, please:"
 read pendrive
-echo "Give my number of cores, which you want to use to compilation:"
+echo "Give my number of cores, which you want to use for compilation:"
 read cores
-apt-get -y update
-apt-get -y upgrade
+#apt-get -y update
+#apt-get -y upgrade
 apt-get -y install make gcc git libncurses5-dev
 cd $pendrive
 echo "Start linux kernel source downloading."
 git clone --depth 1 git://github.com/raspberrypi/linux.git
 cp $pendrive/.config $pendrive/linux
-echo "Linux kernel source downloaded."
-echo "Configuration file (.config) copied to linux kernel source."
-echo "Start tools downloading."
+echo "Linux kernel source was downloaded."
+echo "Configuration file (.config) was copied to linux kernel source."
+echo "Start tools downloading and installing."
 cd /usr/src
 git clone --depth 1 git://github.com/raspberrypi/tools.git
 apt-get -y install gcc-arm-linux-gnueabihf
 apt-get -y install ia32-libs
-echo "Tools downloaded."
+echo "Tools was downloaded and installed."
 cd tools/arm-bcm2708
 cp -r arm-bcm2708hardfp-linux-gnueabi /
 cd /
 mv arm-bcm2708hardfp-linux-gnueabi raspbian
-echo "Cross-Compilator installed."
+echo "Cross-Compilator was installed."
 cd $pendrive/linux
 make ARCH=arm CROSS_COMPILE=/raspbian/bin/arm-bcm2708hardfp-linux-gnueabi- menuconfig
 make -j"$cores" ARCH=arm CROSS_COMPILE=/raspbian/bin/arm-bcm2708hardfp-linux-gnueabi-
-echo "Kernel - done."
+echo "Kernel was compiled."
 make modules -j"$cores" ARCH=arm CROSS_COMPILE=/raspbian/bin/arm-bcm2708hardfp-linux-gnueabi-
-echo "Modules - done."
-echo "Kernel preparation."
+echo "Modules was compiled."
+echo "Kernel is preparating for use."
 cd /usr/src/tools/mkimage
 ./imagetool-uncompressed.py /usr/src/linux/arch/arm/boot/zImage
 cp kernel.img $pendrive
-echo "Kernel file (kernel.img) copied to pendrive."
-echo "Cleaning"
-rm -r $pendrive/linux
+echo "Kernel file (kernel.img) was copied to pendrive."
+echo "Cleaning."
 rm -r /raspbian
 rm -r /usr/src/tools
-echo "Done"
+echo "Done."
 echo "You can install kernel and modules on Raspbian now."
 echo "Good luck."
 
