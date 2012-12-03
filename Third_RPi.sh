5167d28096974e7e4823579ca935faa28f3f7147
piotr-e <piotr-e@wp.pl>
2012-12-03 21:14:34 +0100

    Update Third_RPi.sh

--- Third_RPi.sh
+++ Third_RPi.sh
@@ -6,15 +6,15 @@ echo "Plug your pendrive with your kernel to Raspberry Pi."
 echo "Give my path to your pendrive:"
 read pendrive
 cp $pendrive/kernel.img /boot
-echo "Kernel file (kernel.img) installed."
+echo "Kernel file (kernel.img) was  installed."
 echo "Start linux kernel files copying to Raspbian."
 cp -r $pendrive/linux /usr/src
-echo "Kernel linux files copied to Raspbian."
+echo "Kernel linux files wad copied to Raspbian."
 cd /usr/src/linux
-echo "Modules installing."
+echo "Modules are installing."
 make ARCH=arm CROSS_COMPILE=/cross/bin/arm-bcm2708hardfp-linux-gnueabi- modules_install INSTALL_MOD_PATH=/
 echo "Done."
-echo "Kernel and modules installed."
+echo "Kernel and modules were installed."
 uname -a
 
 
