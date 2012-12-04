ba29b0a7a362f17d44ecc40ce95a9050ab0f2026
piotr-e <piotr-e@wp.pl>
2012-12-04 18:52:03 +0100

    Update First_RPi.sh

--- First_RPi.sh
+++ First_RPi.sh
@@ -5,7 +5,7 @@ echo "This is script, which it prepares Raspbian for kernel cross-compile."
 echo "Give me path to your pendrive, please:"
 read pendrive
 cd /usr/src
-git clone git://github.com/raspberrypi/firmware.git
+git clone --depth 1 git://github.com/raspberrypi/firmware.git
 cd firmware/boot
 cp fixup.dat bootcode.bin loader.bin start.elf start_cd.elf fixup_cd.dat kernel_cutdown.img kernel_emergency.img /boot/
 echo "Firmware was updated."
@@ -14,7 +14,7 @@ echo "Config file was copied to pendrive."
 apt-get install -y make gcc libncurses5-dev
 rm -r $pendrive/linux
 echo "Old linux source was removed."
-git clone git://github.com/raspberrypi/tools.git
+git clone --depth 1 git://github.com/raspberrypi/tools.git
 cd tools/arm-bcm2708
 echo "Cross-Compiler is preparing."
 cp -r arm-bcm2708hardfp-linux-gnueabi /
