818b6285a9868becb70e0302cad29d292c7dc2c7
piotr-e <piotr-e@wp.pl>
2012-12-05 15:58:58 +0100

    Update Second_PC.sh

--- Second_PC.sh
+++ Second_PC.sh
@@ -7,21 +7,19 @@ echo "Give my path to your pendrive, please:"
 read pendrive
 echo "Give my number of cores, which you want to use for compilation:"
 read cores
-#apt-get -y update
-#apt-get -y upgrade
 apt-get -y install make gcc git libncurses5-dev
 cd $pendrive
 echo "Start linux kernel source downloading."
 git clone --depth 1 git://github.com/raspberrypi/linux.git
 mv $pendrive/.config $pendrive/linux/
-echo "Linux kernel source was downloaded."
+echo "Linux kernel source downloaded."
 echo "Configuration file (.config) was copied to linux kernel source."
 echo "Start tools downloading and installing."
 cd /usr/src
 git clone --depth 1 git://github.com/raspberrypi/tools.git
 apt-get -y install gcc-arm-linux-gnueabihf
 apt-get -y install ia32-libs
-echo "Tools was downloaded and installed."
+echo "Tools downloaded and installed."
 cd tools/arm-bcm2708
 cp -r arm-bcm2708hardfp-linux-gnueabi /
 cd /
@@ -30,9 +28,9 @@ echo "Cross-Compilator was installed."
 cd $pendrive/linux
 make ARCH=arm CROSS_COMPILE=/raspbian/bin/arm-bcm2708hardfp-linux-gnueabi- menuconfig
 make -j"$cores" ARCH=arm CROSS_COMPILE=/raspbian/bin/arm-bcm2708hardfp-linux-gnueabi-
-echo "Kernel was compiled."
+echo "Kernel compiled."
 make modules -j"$cores" ARCH=arm CROSS_COMPILE=/raspbian/bin/arm-bcm2708hardfp-linux-gnueabi-
-echo "Modules was compiled."
+echo "Modules compiled."
 echo "Kernel is preparating for use."
 cd /usr/src/tools/mkimage
 ./imagetool-uncompressed.py /usr/src/linux/arch/arm/boot/zImage
