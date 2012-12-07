a8dae684006328e04cf5322996291d57104234f0
piotr-e <piotr-e@wp.pl>
2012-12-07 13:31:05 +0100

    Update Cross_comp._to_SD.sh

--- Cross_comp._to_SD.sh
+++ Cross_comp._to_SD.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
 echo "Hello $USER."
-echo "This is script, which it prepares Raspbian for kernel cross-compile."
-echo "Give me path to your pendrive, please:"
-read pendrive
+echo "This is script, which it prepares PC to cross compiling of Raspbian kernel."
+echo "Plug in SD card to PC."
+echo "Give my full path to SD card with Raspbian file system:"
+read sd_card
 cd /usr/src
-echo "Firmware is updating, it may takes several minutes."
-git clone --depth 1 git://github.com/raspberrypi/firmware.git
-cd firmware/boot
-cp fixup.dat bootcode.bin start.elf start_cd.elf fixup_cd.dat kernel_cutdown.img kernel_emergency.img /boot/
-echo "Firmware updated."
-zcat /proc/config.gz > $pendrive/.config
-echo "Config file copied to pendrive."
+git clone --depth git://github.com/raspberrypi/linux.git
+zcat $sd_card/proc/config.gz > /usr/src/.config
 apt-get install -y make gcc libncurses5-dev
-echo "Old linux source is removing."
-rm -r $pendrive/linux
-echo "Old linux source removed."
-echo "Necessary tools are dowloading."
 cd /usr/src
 git clone --depth 1 git://github.com/raspberrypi/tools.git
 cd tools/arm-bcm2708
-echo "Necessary tools downloaded."
 echo "Cross-Compiler is preparing."
 cp -r arm-bcm2708hardfp-linux-gnueabi /
 cd /
 mv arm-bcm2708hardfp-linux-gnueabi cross
-echo "Cross-Compiler prepared."
-echo "Cleaning."
 rm -r /usr/src/tools
-rm -r /usr/src/firmware
 echo "Done."
-echo "You can compile kernel on PC by running Second_PC.sh. "
+echo "You can plug SD card to Raspberry Pi and running. "
 echo "Good luck."
