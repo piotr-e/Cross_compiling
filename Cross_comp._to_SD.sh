7a3b4df8d8818e8387a76f31527414df2389baa0
piotr-e <piotr-e@wp.pl>
2012-12-07 13:33:21 +0100

    Update Cross_comp._to_SD.sh

--- Cross_comp._to_SD.sh
+++ Cross_comp._to_SD.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo " "
 echo "Hello $USER."
 echo "This is script, which it prepares PC to cross compiling of Raspbian kernel."
 echo "Plug in SD card to PC."
@@ -12,7 +13,6 @@ apt-get install -y make gcc libncurses5-dev
 cd /usr/src
 git clone --depth 1 git://github.com/raspberrypi/tools.git
 cd tools/arm-bcm2708
-echo "Cross-Compiler is preparing."
 cp -r arm-bcm2708hardfp-linux-gnueabi /
 cd /
 mv arm-bcm2708hardfp-linux-gnueabi cross
